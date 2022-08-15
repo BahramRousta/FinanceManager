fc635ba732ea4fdc36bbfb21c3512fceb56c30d5
BahramRousta <rousta.bahram@gmail.com>
2022-08-15 21:21:10 +0430

    add TokenRefreshView to get new access token

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import CustomUser, Otp
 
-# Register your models here.
+
+class CustomUserAdmin(admin.ModelAdmin):
+    pass
--- accounts/urls.py
+++ accounts/urls.py
@@ -3,10 +3,16 @@ from .views import (
     RegisterApiView,
     LogOut
 )
+from rest_framework_simplejwt.views import (
+    TokenRefreshView,
+)
 
 app_name = 'accounts'
 
 urlpatterns = [
     path('register/', RegisterApiView.as_view(), name='register'),
     path('logout/', LogOut.as_view(), name='logout'),
+
+    # path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
